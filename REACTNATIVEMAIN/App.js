3dc557fa196930e97d088393d444244b8ae0e2fb
VU Tuan Trung <trung.lespace@gmail.com>
2020-12-20 19:55:01 +0100

    Initialize main project

---
+++ REACTNATIVEMAIN/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, StyleSheet, } from 'react-native';
+import { Colors, } from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
